06fe01317cc91984de738836bd331dd7b66e16c5
dunhanson <dunhanson@gmail.com>
2020-12-18 09:25:00 +0800

    first commit

---
+++ src/main/java/site/dunhanson/redisson/spring/boot/RedissonSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package site.dunhanson.redisson.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissonSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/dunhanson/redisson/spring/boot/RedissonSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dunhanson.redisson.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
